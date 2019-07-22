4ac41b4bc878466163602337626d7d39f6b9aa5d
Illu <illu@notvalid.de>
2019-07-22 21:46:01 +0200

    New Gambit Condition
    - Added "Not in Instance"
    - Conditions need a UI reshuffel

---
+++ Magitek/Gambits/Conditions/NotInInstanceCondition.cs
@@ -0,0 +1,17 @@
+﻿using ff14bot.Managers;
+using ff14bot.Objects;
+
+namespace Magitek.Gambits.Conditions
+{
+    public class NotInInstanceCondition : GambitCondition
+    {
+        public NotInInstanceCondition() : base(GambitConditionTypes.NotInInstance)
+        {
+        }
+
+        public override bool Check(GameObject gameObject = null)
+        {
+            return !DutyManager.InInstance;
+        }
+    }
+}
