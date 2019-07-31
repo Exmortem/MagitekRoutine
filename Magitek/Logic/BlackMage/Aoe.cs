57e96723f9976886d26599ecd97ea44de70d0781
David Connelly <dconnelly@live.com>
2019-07-31 08:46:11 -0600

    Remove language and version

--- Magitek/Logic/BlackMage/Aoe.cs
+++ Magitek/Logic/BlackMage/Aoe.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Buddy.Coroutines;
 using ff14bot;
+using ff14bot.Helpers;
 using ff14bot.Managers;
 using Magitek.Extensions;
 using Magitek.Models.BlackMage;
