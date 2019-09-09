a0d39e36cb37eb1aed5a4adb66bc32194540e207
Galio <faprivado4@gmail.com>
2019-09-09 22:42:20 +0100

    Fixed Low Level Bootshine.

--- Magitek/Logic/Monk/Buff.cs
+++ Magitek/Logic/Monk/Buff.cs
@@ -147,17 +147,18 @@ namespace Magitek.Logic.Monk
 
         public static async Task<bool> FormShift()
         {
-            if (Core.Me.ClassLevel < 52)
-                return false;
+            if (Core.Me.ClassLevel > 52 && Core.Me.InCombat)
+            {
 
-            if (Core.Me.InCombat && !Core.Me.HasTarget && MonkSettings.Instance.UseAutoFormShift && ActionResourceManager.Monk.Timer.Seconds < 6 && ActionResourceManager.Monk.GreasedLightning == 4)
-                return await Spells.FormShift.Cast(Core.Me);
+                if (!Core.Me.HasTarget && MonkSettings.Instance.UseAutoFormShift && ActionResourceManager.Monk.Timer.Seconds < 6 && ActionResourceManager.Monk.GreasedLightning == 4)
+                    return await Spells.FormShift.Cast(Core.Me);
 
-            if (MonkSettings.Instance.AutoFormShiftStopCoeurl && !Core.Me.HasAura(Auras.CoeurlForm) && ActionResourceManager.Monk.GreasedLightning == 4)
-                return await Spells.FormShift.Cast(Core.Me);
+                if (MonkSettings.Instance.AutoFormShiftStopCoeurl && !Core.Me.HasAura(Auras.CoeurlForm) && ActionResourceManager.Monk.GreasedLightning == 4)
+                    return await Spells.FormShift.Cast(Core.Me);
 
-            if (MonkSettings.Instance.AutoFormShiftStopRaptor && !Core.Me.HasAura(Auras.RaptorForm) && ActionResourceManager.Monk.GreasedLightning == 4)
-                return await Spells.FormShift.Cast(Core.Me);
+                if (MonkSettings.Instance.AutoFormShiftStopRaptor && !Core.Me.HasAura(Auras.RaptorForm) && ActionResourceManager.Monk.GreasedLightning == 4)
+                    return await Spells.FormShift.Cast(Core.Me);
+            }
 
             if (Core.Me.HasAura(Auras.PerfectBalance))
                 return false;
