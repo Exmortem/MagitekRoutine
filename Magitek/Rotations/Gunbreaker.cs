a4ba23d0b0fb83207384ee981c5c774a29094cb6
Omninewb <newb23@gmail.com>
2019-11-11 10:47:44 -0700

    GNRL: Updated global variables to update properly.

--- Magitek/Rotations/Gunbreaker.cs
+++ Magitek/Rotations/Gunbreaker.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using ff14bot;
 using ff14bot.Managers;
 using Magitek.Extensions;
