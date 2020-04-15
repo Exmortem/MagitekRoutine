ceb4084ca4e3ddf5cf8cd55c41c33d0ab77d289f
lokibunny <63686627+lokibunny@users.noreply.github.com>
2020-04-15 02:18:15 -0400

    Cleaning up RDM
    Added more logic to Verfire to see if that helps dead space in rotation.
    
    Borrowed logic from SMN to use with moulinet, stopped it from going off as soon as we have 20 B/W mana.

--- Magitek/Logic/RedMage/Aoe.cs
+++ Magitek/Logic/RedMage/Aoe.cs
@@ -7,6 +7,7 @@ using ff14bot.Managers;
 using Magitek.Extensions;
 using Magitek.Models.RedMage;
 using Magitek.Utilities;
+using Magitek.Models.QueueSpell;
 using static ff14bot.Managers.ActionResourceManager.RedMage;
 
 namespace Magitek.Logic.RedMage
@@ -23,8 +24,9 @@ namespace Magitek.Logic.RedMage
 
             if (Core.Me.CurrentTarget.EnemiesNearby(5).Count() < RedMageSettings.Instance.ScatterEnemies)
                 return false;
-
-            return await Spells.Scatter.Cast(Core.Me.CurrentTarget);
+			
+			else
+				return await Spells.Scatter.Cast(Core.Me.CurrentTarget);
         }
 
         public static async Task<bool> ContreSixte()
@@ -37,13 +39,14 @@ namespace Magitek.Logic.RedMage
 
             if (Core.Me.HasAura(Auras.Dualcast))
                 return false;
-
-            return await Spells.ContreSixte.Cast(Core.Me.CurrentTarget);
+			
+			else
+				return await Spells.ContreSixte.Cast(Core.Me.CurrentTarget);
         }
-
+		
         public static async Task<bool> Moulinet()
         {
-            if (!RedMageSettings.Instance.UseMelee)
+			if (!RedMageSettings.Instance.UseMelee)
                 return false;
 
             if (!RedMageSettings.Instance.Moulinet)
@@ -51,47 +54,58 @@ namespace Magitek.Logic.RedMage
 
             if (BlackMana < 20 || WhiteMana < 20)
                 return false;
-
-            if (Combat.Enemies.Count(r => r.InView() && r.Distance(Core.Me) <= 6 + r.CombatReach) < RedMageSettings.Instance.MoulinetEnemies)
+			
+			if (Combat.Enemies.Count(r => r.InView() && r.Distance(Core.Me) <= 6 + r.CombatReach) < RedMageSettings.Instance.MoulinetEnemies)
                 return false;
 			
-			if (Combat.Enemies.Count(r => r.InView() && r.Distance(Core.Me) <= 6 + r.CombatReach) >= 4 && BlackMana >= 50 && WhiteMana >= 50 && Spells.Manafication.Cooldown == TimeSpan.Zero && RedMageSettings.Instance.Manafication)
-			{
+            if (Combat.Enemies.Count(r => r.InView() && r.Distance(Core.Me) <= 6 + r.CombatReach) >= 4 && BlackMana >= 50 && WhiteMana >= 50 && Spells.Manafication.Cooldown == TimeSpan.Zero && RedMageSettings.Instance.Manafication)
+            {
+                Logger.Error("Bursting Moulinet");
+
+                SpellQueueLogic.SpellQueue.Clear();
+                SpellQueueLogic.Timeout.Start();
+                SpellQueueLogic.CancelSpellQueue = () => SpellQueueLogic.Timeout.ElapsedMilliseconds > 18000;
 				if (BlackMana >= 90 && WhiteMana >= 90 && RedMageSettings.Instance.Embolden)
 				{
-					return await Spells.Embolden.Cast(Core.Me);
+					SpellQueueLogic.SpellQueue.Enqueue(new QueueSpell { Spell = Spells.Embolden, TargetSelf = true, Wait = new QueueSpellWait() { Name = "Wait for Embolden", Check = () => ActionManager.CanCast(Spells.Embolden, null), WaitTime = 1500, EndQueueIfWaitFailed = true }, });
 				}
 				
 				while (BlackMana >= 70 && WhiteMana >= 70)
 				{
-					return await Spells.Moulinet.Cast(Core.Me.CurrentTarget);
+					SpellQueueLogic.SpellQueue.Enqueue(new QueueSpell { Spell = Spells.Moulinet, Wait = new QueueSpellWait() { Name = "Wait for Moulinet", Check = () => ActionManager.CanCast(Spells.Moulinet, null), WaitTime = 1500, EndQueueIfWaitFailed = true }, });
 				}
 				
 				if (!Core.Me.HasAura(Auras.Manafication))
 				{
-					return await Spells.Manafication.Cast(Core.Me);
+					SpellQueueLogic.SpellQueue.Enqueue(new QueueSpell { Spell = Spells.Manafication, TargetSelf = true, Wait = new QueueSpellWait() { Name = "Wait for Manafication", Check = () => ActionManager.CanCast(Spells.Manafication, null), WaitTime = 1500, EndQueueIfWaitFailed = true }, });
 				}
 				
 				while (BlackMana >= 20 && WhiteMana >= 20)
 				{
-					return await Spells.Moulinet.Cast(Core.Me.CurrentTarget);
+					SpellQueueLogic.SpellQueue.Enqueue(new QueueSpell { Spell = Spells.Moulinet, Wait = new QueueSpellWait() { Name = "Wait for Moulinet", Check = () => ActionManager.CanCast(Spells.Moulinet, null), WaitTime = 1500, EndQueueIfWaitFailed = true }, });
 				}
 				
 				if (Spells.Swiftcast.Cooldown == TimeSpan.Zero && !Core.Me.HasAura(Auras.Swiftcast))
 				{
-					return await Spells.Swiftcast.CastAura(Core.Me, Auras.Swiftcast);
+					SpellQueueLogic.SpellQueue.Enqueue(new QueueSpell { Spell = Spells.Swiftcast, TargetSelf = true, Wait = new QueueSpellWait() { Name = "Wait for Swiftcast", Check = () => ActionManager.CanCast(Spells.Swiftcast, null), WaitTime = 1500, EndQueueIfWaitFailed = true }, });
 				}
 				
-				if (Core.Me.HasAura(Auras.Swiftcast)
+				if (Core.Me.HasAura(Auras.Swiftcast))
 				{
-					return await Spells.Impact.Cast(Core.Me.CurrentTarget);
+					SpellQueueLogic.SpellQueue.Enqueue(new QueueSpell { Spell = Spells.Impact, Wait = new QueueSpellWait() { Name = "Wait for Impact", Check = () => ActionManager.CanCast(Spells.Impact, null), WaitTime = 1500, EndQueueIfWaitFailed = true }, });
 				}
-			}
+				
+				else
+					return false;
+            }
 			
 			if (Combat.Enemies.Count(r => r.InView() && r.Distance(Core.Me) <= 6 + r.CombatReach) == 3 && BlackMana >= 90 && WhiteMana >= 90)
-				return await Spells.Moulinet.Cast(Core.Me.CurrentTarget);
-			
-            return await Spells.Moulinet.Cast(Core.Me.CurrentTarget);
+			{
+				SpellQueueLogic.SpellQueue.Enqueue(new QueueSpell { Spell = Spells.Moulinet, Wait = new QueueSpellWait() { Name = "Wait for Moulinet", Check = () => ActionManager.CanCast(Spells.Moulinet, null), WaitTime = 1500, EndQueueIfWaitFailed = true }, });
+			}
+            
+			else
+				return false;
         }
 
         public static async Task<bool> Veraero2()
@@ -104,8 +118,9 @@ namespace Magitek.Logic.RedMage
 
             if (Core.Me.CurrentTarget.EnemiesNearby(5).Count() < RedMageSettings.Instance.Ver2Enemies)
                 return false;
-
-            return await Spells.Veraero2.Cast(Core.Me.CurrentTarget);
+			
+			else
+				return await Spells.Veraero2.Cast(Core.Me.CurrentTarget);
         }
 
         public static async Task<bool> Verthunder2()
@@ -118,8 +133,9 @@ namespace Magitek.Logic.RedMage
 
             if (Core.Me.CurrentTarget.EnemiesNearby(5).Count() < RedMageSettings.Instance.Ver2Enemies)
                 return false;
-
-            return await Spells.Verthunder2.Cast(Core.Me.CurrentTarget);
+			
+			else
+				return await Spells.Verthunder2.Cast(Core.Me.CurrentTarget);
         }
     }
 }
