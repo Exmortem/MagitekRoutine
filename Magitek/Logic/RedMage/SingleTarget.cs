c251de2485c6802cc8fdc42bea1b1f6ecb7f56ef
EighthMonth <15336003+EighthMonth@users.noreply.github.com>
2020-05-22 08:20:48 -0700

    RDM: Don't use Reprise during combo
    - Don't use reprise during combo
    - Also fixed merge bug

--- Magitek/Logic/RedMage/SingleTarget.cs
+++ Magitek/Logic/RedMage/SingleTarget.cs
@@ -188,7 +188,7 @@ namespace Magitek.Logic.RedMage
                         return false;
 
                     //Don't swiftcast right before a combo, it'll mess up our procs, or in a combo, it'll mess up the combo
-                    if (ReadyForCombo(BlackMana+11, WhiteMana))
+                    if (ComboInProgress || ReadyForCombo())
                         return false;
 
                     if (!RedMageRoutines.CanWeave)
@@ -210,7 +210,7 @@ namespace Magitek.Logic.RedMage
 
             //Check if we need to clean up our procs before the combo
             //TODO: We also need to prep if we're going to manify right after this
-            if (ReadyForCombo())
+            if (ReadyForCombo(BlackMana + 11, WhiteMana))
             {
                 if (PrepForComboWithVerthunder())
                     return await Spells.Verthunder.Cast(Core.Me.CurrentTarget);
@@ -464,6 +464,9 @@ namespace Magitek.Logic.RedMage
         public static async Task<bool> Reprise()
         {
             if (BlackMana < 5 || WhiteMana < 5)
+                return false;
+
+            if (ComboInProgress)
                 return false;
 
             if (Core.Me.CurrentTarget.Distance(Core.Me) > Spells.Reprise.Range + Core.Me.CurrentTarget.CombatReach + Core.Me.CombatReach)
