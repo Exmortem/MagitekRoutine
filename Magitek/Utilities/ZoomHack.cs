7ae89b5d71026b533a5d4eca2cee17b675d0fbac
fioxpt <faprivado4@gmail.com>
2020-08-12 10:53:19 +0100

    Zoomhack Offset Update

--- Magitek/Utilities/ZoomHack.cs
+++ Magitek/Utilities/ZoomHack.cs
@@ -20,7 +20,7 @@ namespace Magitek.Utilities
 
             try
             {
-                Core.Memory.Write(Core.Memory.Read<IntPtr>(Core.Memory.Process.MainModule.BaseAddress + 0x1C63F80) + 0x11c, BaseSettings.Instance.ZoomHack ? 200f : 20f);
+                Core.Memory.Write(Core.Memory.Read<IntPtr>(Core.Memory.Process.MainModule.BaseAddress + 0x1D09BA0) + 0x11c, BaseSettings.Instance.ZoomHack ? 200f : 20f);
                 _isEnabled = BaseSettings.Instance.ZoomHack;
             }
             catch (Exception)
