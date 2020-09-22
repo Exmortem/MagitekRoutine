69eaec18eeae836c4b207f899c85032633a8868d
fioxpt <faprivado4@gmail.com>
2020-09-22 14:26:51 +0100

    TTD Fix for MCH

--- Magitek/Logic/Machinist/Pet.cs
+++ Magitek/Logic/Machinist/Pet.cs
@@ -40,7 +40,7 @@ namespace Magitek.Logic.Machinist
                     return false;
             }
 
-            if (Core.Me.CurrentTarget.CombatTimeLeft() <= 5000)
+            if (Core.Me.CurrentTarget.CombatTimeLeft() <= 5)
                 return false;
 
             return await MachinistGlobals.RookQueenPet.Cast(Core.Me);
@@ -51,7 +51,7 @@ namespace Magitek.Logic.Machinist
             if (!MachinistSettings.Instance.UseRookQueenOverdrive)
                 return false;
 
-            if (Core.Me.CurrentTarget.CombatTimeLeft() <= 2000 && Core.Me.CurrentTarget.CurrentHealthPercent < 2)
+            if (Core.Me.CurrentTarget.CombatTimeLeft() <= 2 && Core.Me.CurrentTarget.CurrentHealthPercent < 2)
                 return await MachinistGlobals.RookQueenOverdrive.Cast(Core.Me.CurrentTarget);
 
             return false;
