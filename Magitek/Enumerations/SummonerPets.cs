9249b625441514ea189d352cea3ab64bbe85ba90
lokibunny <63686627+lokibunny@users.noreply.github.com>
2020-12-25 11:12:03 -0500

    [SMN] Low level fix
    Added emerald and topaz carbuncle for use with arcanist prior to getting other summons at level 30.
    
    If emerald or topaz are selected at 30 or above, no pet will be summoned.

--- Magitek/Enumerations/SummonerPets.cs
+++ Magitek/Enumerations/SummonerPets.cs
@@ -5,6 +5,8 @@ namespace Magitek.Enumerations
     public enum SummonerPets
     {
         None = PetType.None,
+        EmeraldCarbuncle = PetType.Emerald_Carbuncle,
+        TopazCarbuncle = PetType.Topaz_Carbuncle,
         Ifrit = PetType.Ifrit_Egi,
         Titan = PetType.Titan_Egi,
         Garuda = PetType.Garuda_Egi
