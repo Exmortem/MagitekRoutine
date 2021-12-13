d480dab50e6736b5fdfe1df7c8ea980faf90fcaf
Illu <illu@notvalid.de>
2021-12-13 22:44:29 +0100

    Reaper
    - Added Stuff

---
+++ Magitek/Models/Reaper/ReaperSettings.cs
@@ -0,0 +1,19 @@
+﻿using Magitek.Enumerations;
+using Magitek.Models.Roles;
+using PropertyChanged;
+using System.ComponentModel;
+using System.Configuration;
+
+namespace Magitek.Models.Reaper
+{
+    [AddINotifyPropertyChangedInterface]
+    public class ReaperSettings : PhysicalDpsSettings, IRoutineSettings
+    {
+        public ReaperSettings() : base(CharacterSettingsDirectory + "/Magitek/Reaper/ReaperSettings.json") { }
+
+        public static ReaperSettings Instance { get; set; } = new ReaperSettings();
+
+        
+
+    }
+}
