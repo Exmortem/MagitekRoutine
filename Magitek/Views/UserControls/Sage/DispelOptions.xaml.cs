e6d424048a52c8b661d0bb38bb0176ec4d92c1b9
Wild Chill <wildchill@fastmail.com>
2021-12-23 13:53:29 -0800

    Initial attempt over Sage Settings UI

---
+++ Magitek/Views/UserControls/Sage/DispelOptions.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Magitek.Views.UserControls.Sage
+{
+    /// <summary>
+    /// Interaction logic for DispelOptions.xaml
+    /// </summary>
+    public partial class DispelOptions : UserControl
+    {
+        public DispelOptions()
+        {
+            InitializeComponent();
+        }
+    }
+}
