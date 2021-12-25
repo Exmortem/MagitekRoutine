e9800d58560216f3357d00e0ea2efaa2823aa003
aalos <aalosb@gmail.com>
2021-12-25 03:34:51 -0600

    Update RDM for EW spells.

--- Magitek/Utilities/Spells.cs
+++ Magitek/Utilities/Spells.cs
@@ -504,6 +504,7 @@ namespace Magitek.Utilities
         public static readonly SpellData Engagement = DataManager.GetSpellData(16527);
         public static readonly SpellData Reprise = DataManager.GetSpellData(16528);
         public static readonly SpellData Scorch = DataManager.GetSpellData(16530);
+        public static readonly SpellData Resolution = DataManager.GetSpellData(25858);
         public static readonly SpellData VerthunderIII = DataManager.GetSpellData(25855);
         public static readonly SpellData VeraeroIII = DataManager.GetSpellData(25856);
         public static readonly SpellData MagickBarrier = DataManager.GetSpellData(25857);
