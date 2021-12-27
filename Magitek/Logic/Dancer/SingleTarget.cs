21feee4ffcc609f5a187abefb576806526f1c8fc
pyromanci <pyromanci@gmail.com>
2021-12-26 23:04:42 -0500

    Dancer - QOL changes
    Dance Step and Dance Finish Gambit Actions Added.
    
    Added Toggleable Experimental Facing and Range Checks.  To single Target spells.
    
    Minor DPS priority tweak.

--- Magitek/Logic/Dancer/SingleTarget.cs
+++ Magitek/Logic/Dancer/SingleTarget.cs
@@ -17,12 +17,27 @@ namespace Magitek.Logic.Dancer
 
             if (Combat.Enemies.Count(r => r.Distance(Core.Me) <= 5 + r.CombatReach) >= DancerSettings.Instance.FanDanceTwoEnemies)
                 return false;
+
+            if (DancerSettings.Instance.UseExpermentalChecks)
+            {
+                if (Core.Me.CurrentTarget.Distance(Core.Me) > Spells.FanDance.Range) return false;
+
+                if (!GameSettingsManager.FaceTargetOnAction && !Core.Me.CurrentTarget.InView()) return false;
+            }
+
             return await Spells.FanDance.Cast(Core.Me.CurrentTarget);
         }
 
         public static async Task<bool> Fountainfall()
         {
             if (!Core.Me.HasAura(Auras.FlourshingFlow) && !Core.Me.HasAura(Auras.FlourshingFountain)) return false;
+
+            if (DancerSettings.Instance.UseExpermentalChecks)
+            {
+                if (Core.Me.CurrentTarget.Distance(Core.Me) > Spells.Fountainfall.Range) return false;
+
+                if (!GameSettingsManager.FaceTargetOnAction && !Core.Me.CurrentTarget.InView()) return false;
+            }
 
             return await Spells.Fountainfall.Cast(Core.Me.CurrentTarget);
         }
@@ -31,6 +46,14 @@ namespace Magitek.Logic.Dancer
         {
             if (!Core.Me.HasAura(Auras.FlourishingSymmetry) && !Core.Me.HasAura(Auras.FlourshingFountain)) return false;
 
+            if (DancerSettings.Instance.UseExpermentalChecks)
+            {
+                if (Core.Me.CurrentTarget.Distance(Core.Me) > Spells.ReverseCascade.Range) return false;
+
+
+                if (!GameSettingsManager.FaceTargetOnAction && !Core.Me.CurrentTarget.InView()) return false;
+            }
+
             return await Spells.ReverseCascade.Cast(Core.Me.CurrentTarget);
         }
 
@@ -38,11 +61,28 @@ namespace Magitek.Logic.Dancer
         {
             if (ActionManager.LastSpell != Spells.Cascade) return false;
 
+            if (DancerSettings.Instance.UseExpermentalChecks)
+            {
+                if (Core.Me.CurrentTarget.Distance(Core.Me) > Spells.Fountain.Range) return false;
+
+                if (!GameSettingsManager.FaceTargetOnAction && !Core.Me.CurrentTarget.InView()) return false;
+            }
+
             return await Spells.Fountain.Cast(Core.Me.CurrentTarget);
         }
 
         public static async Task<bool> Cascade()
         {
+            if (Core.Me.CurrentTarget.Distance(Core.Me) > Spells.Cascade.Range) return false;
+
+            if (DancerSettings.Instance.UseExpermentalChecks)
+            {
+                if (Core.Me.CurrentTarget.Distance(Core.Me) > Spells.Cascade.Range) return false;
+
+                if (!GameSettingsManager.FaceTargetOnAction && !Core.Me.CurrentTarget.InView()) return false;
+
+            }
+
             return await Spells.Cascade.Cast(Core.Me.CurrentTarget);
         }
     }
