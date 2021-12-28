be6a27457f2a6e94fd77659b5ef6d00b74104b5e
Cli0l1n3 <Cli0l1n3@gmail.com>
2021-12-28 10:16:25 +0100

    Fix IsReady method for Spells

--- Magitek/Extensions/SpellDataExtensions.cs
+++ Magitek/Extensions/SpellDataExtensions.cs
@@ -239,7 +239,7 @@ namespace Magitek.Extensions
 
         public static bool IsReady(this SpellData spell, int remainingTimeInMs = 0)
         {
-            return spell.Cooldown.TotalMilliseconds <= remainingTimeInMs;
+            return IsKnown(spell) && spell.Cooldown.TotalMilliseconds <= remainingTimeInMs;
         }
 
         public static uint AdjustedSpellCostBlm(this SpellData spell)
