c88538f1734c96bfd5923229516828d9803fe764
Nick <2124370+Dubsys@users.noreply.github.com>
2022-01-17 17:27:34 +1100

    Invuln
    STRESS

--- Magitek/Utilities/Tracking.cs
+++ Magitek/Utilities/Tracking.cs
@@ -68,7 +68,7 @@ namespace Magitek.Utilities
                 if (!unit.ValidAttackUnit())
                     continue;
 
-                if (!unit.NotInvulnerable)
+                if (!unit.NotInvulnerable())
                     continue;
 
                 if (!unit.IsTargetable)
@@ -158,7 +158,7 @@ namespace Magitek.Utilities
             }
 
 
-            var removeDpsUnits = EnemyInfos.Where(r => !_enemyCache.Contains(r.Unit) || r.Unit == null || r.Unit.IsDead || !r.Unit.IsValid).ToArray();
+            var removeDpsUnits = EnemyInfos.Where(r => !_enemyCache.Contains(r.Unit) || r.Unit == null || r.Unit.HasAnyAura(Auras.Invincibility) || r.Unit.IsDead ||  !r.Unit.IsValid).ToArray();
 
             foreach (var unit in removeDpsUnits)
             {
