879e679ffe5cff5cd630add277fb89f5a1cbcfca
xil64 <bobsam1576@gmail.com>
2022-01-30 16:29:12 -0800

    add Ruby/Topaz/Emerald Ruin to Smn Rotation (for low level sync)

--- Magitek/Logic/Summoner/SingleTarget.cs
+++ Magitek/Logic/Summoner/SingleTarget.cs
@@ -65,22 +65,31 @@ namespace Magitek.Logic.Summoner
                         }
                     case SmnResources.ActivePetType.Ifrit when Spells.RubyRuinIII.IsKnown():
                         return await Spells.RubyRuinIII.Cast(Core.Me.CurrentTarget);
-                    case SmnResources.ActivePetType.Ifrit:
+                    case SmnResources.ActivePetType.Ifrit when Spells.RubyRuinII.IsKnown():
                         return await Spells.RubyRuinII.Cast(Core.Me.CurrentTarget);
+                    case SmnResources.ActivePetType.Ifrit:
+                        return await Spells.RubyRuin.Cast(Core.Me.CurrentTarget);
+                    
 
                     case SmnResources.ActivePetType.Titan when Spells.TopazRite.IsKnown():
                         return await Spells.TopazRite.Cast(Core.Me.CurrentTarget);
                     case SmnResources.ActivePetType.Titan when Spells.TopazRuinIII.IsKnown():
                         return await Spells.TopazRuinIII.Cast(Core.Me.CurrentTarget);
-                    case SmnResources.ActivePetType.Titan:
+                    case SmnResources.ActivePetType.Titan when Spells.TopazRuinII.IsKnown():
                         return await Spells.TopazRuinII.Cast(Core.Me.CurrentTarget);
+                    case SmnResources.ActivePetType.Titan:
+                        return await Spells.TopazRuin.Cast(Core.Me.CurrentTarget);
+                        
+                    
 
                     case SmnResources.ActivePetType.Garuda when Spells.EmeraldRite.IsKnown():
                         return await Spells.EmeraldRite.Cast(Core.Me.CurrentTarget);
                     case SmnResources.ActivePetType.Garuda when Spells.EmeraldRuinIII.IsKnown():
                         return await Spells.EmeraldRuinIII.Cast(Core.Me.CurrentTarget);
-                    case SmnResources.ActivePetType.Garuda:
+                    case SmnResources.ActivePetType.Garuda when Spells.EmeraldRuinII.IsKnown():
                         return await Spells.EmeraldRuinII.Cast(Core.Me.CurrentTarget);
+                    case SmnResources.ActivePetType.Garuda:
+                        return await Spells.EmeraldRuin.Cast(Core.Me.CurrentTarget);
                 }
 
             if (Core.Me.CurrentJob == ClassJobType.Arcanist || Core.Me.ClassLevel <= 30)
