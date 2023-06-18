a709c8b3f71662877d061f64f39e4ce90ba3cb68
Illu <illu@notvalid.de>
2023-06-18 16:31:08 +0200

    NIN Overhaul
    + Added Hellfrog Medium

--- Magitek/Logic/Ninja/Aoe.cs
+++ Magitek/Logic/Ninja/Aoe.cs
@@ -12,7 +12,22 @@ namespace Magitek.Logic.Ninja
     internal static class Aoe
     {
         
+        public static async Task<bool> HellfrogMedium()
+        {
 
+            if (Core.Me.ClassLevel < 62)
+                return false;
+
+            if (!Spells.HellfrogMedium.IsKnown())
+                return false;
+
+            //dumping ninki before mug is missung
+            if (ActionResourceManager.Ninja.NinkiGauge < 90)
+                return false;
+
+            //Smart Target Logic needs to be addded
+            return await Spells.HellfrogMedium.Cast(Core.Me.CurrentTarget);
+        }
 
     }
 }
