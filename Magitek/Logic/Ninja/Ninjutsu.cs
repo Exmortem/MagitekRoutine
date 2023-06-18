27051ca7fb9c9d950c441021bb5bac46c2bcb44e
Illu <illu@notvalid.de>
2023-06-18 20:10:06 +0200

    NIN Overhaul
    + Added Suiton

--- Magitek/Logic/Ninja/Ninjutsu.cs
+++ Magitek/Logic/Ninja/Ninjutsu.cs
@@ -12,7 +12,9 @@ namespace Magitek.Logic.Ninja
 {
     internal static class Ninjutsu
     {
-        
+
+        #region Utility Ninjustsus
+
         public static async Task<bool> Huton()
         {
 
@@ -92,6 +94,87 @@ namespace Magitek.Logic.Ninja
             return false;
 
         }
+
+        public static async Task<bool> Suiton()
+        {
+
+            //if (!NinjaSettings.Instance.UseHuton)
+            //    return false;
+
+            if (Core.Me.ClassLevel < 45)
+                return false;
+
+            if (!Spells.Jin.IsKnown())
+                return false;
+
+            if (Core.Me.HasAura(Auras.TenChiJin) || Core.Me.HasAura(Auras.Kassatsu))
+                return false;
+
+            if (Spells.TrickAttack.AdjustedCooldown >= new TimeSpan(0, 0, 12))
+                return false;
+
+            if (Core.Me.HasMyAura(Auras.Suiton))
+                return false;
+
+            switch (NinjaRoutine.UsedMudras.Count)
+            {
+
+                case 0:
+                    if (await Spells.Ten.Cast(Core.Me))
+                    {
+                        NinjaRoutine.UsedMudras.Add(Spells.Ten);
+                        return true;
+                    }
+                    return false;
+
+                case 1:
+                    if (NinjaRoutine.UsedMudras.Last() == Spells.Ten)
+                    {
+                        if (await Spells.Chi.Cast(Core.Me))
+                        {
+                            NinjaRoutine.UsedMudras.Add(Spells.Chi);
+                            return true;
+                        }
+                    }
+                    else if (NinjaRoutine.UsedMudras.Last() == Spells.Chi)
+                    {
+                        if (await Spells.Ten.Cast(Core.Me))
+                        {
+                            NinjaRoutine.UsedMudras.Add(Spells.Chi);
+                            return true;
+                        }
+                    }
+                    return false;
+
+                case 2:
+
+                    if (!NinjaRoutine.UsedMudras.Contains(Spells.Jin))
+                    {
+                        if (await Spells.Jin.Cast(Core.Me))
+                        {
+                            NinjaRoutine.UsedMudras.Add(Spells.Jin);
+                            return true;
+                        }
+                    }
+                    return false;
+
+                case 3:
+                    if (await Spells.Ninjutsu.Cast(Core.Me.CurrentTarget))
+                    {
+                        NinjaRoutine.UsedMudras.Clear();
+                        return true;
+                    }
+                    return false;
+
+                default:
+                    break;
+            }
+
+            return false;
+
+        }
+
+        #endregion
 
         #region TenChiJin
 
