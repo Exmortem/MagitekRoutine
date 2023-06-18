b137d279949b73fe1c97dea70a479b88be3919d3
Illu <illu@notvalid.de>
2023-06-18 16:58:11 +0200

    NIN Overhaul
    + Added Magitek Resource Manager until Mashtag updates the Ninki Gauge offset

--- Magitek/Logic/Ninja/Aoe.cs
+++ Magitek/Logic/Ninja/Aoe.cs
@@ -5,7 +5,6 @@ using Magitek.Models.Ninja;
 using Magitek.Utilities;
 using System.Linq;
 using System.Threading.Tasks;
-using static ff14bot.Managers.ActionResourceManager.Ninja;
 
 namespace Magitek.Logic.Ninja
 {
@@ -22,7 +21,7 @@ namespace Magitek.Logic.Ninja
                 return false;
 
             //dumping ninki before mug is missung
-            if (ActionResourceManager.Ninja.NinkiGauge < 90)
+            if (MagitekActionResourceManager.Ninja.NinkiGauge < 90)
                 return false;
 
             //Smart Target Logic needs to be addded
