0f497b957fd78caa2493960ac4df5391ccc44e29
Illu <illu@notvalid.de>
2023-06-20 20:56:49 +0200

    NIN - Pre Pull Hide
    + Added Pre Pull Hide Logic

--- Magitek/Logic/Ninja/Utility.cs
+++ Magitek/Logic/Ninja/Utility.cs
@@ -1,21 +1,27 @@
-﻿using Clio.Utilities.Helpers;
-using ff14bot;
+﻿using ff14bot;
 using ff14bot.Managers;
 using Magitek.Extensions;
-using Magitek.Models.Ninja;
-using Magitek.Models.Roles;
-using Magitek.Toggles;
 using Magitek.Utilities;
 using System.Linq;
 using System.Threading.Tasks;
-using static ff14bot.Managers.ActionResourceManager.Ninja;
 
 namespace Magitek.Logic.Ninja
 {
     internal static class Utility
     {
 
+        public static async Task<bool> PrePullHide()
+        {
 
+            if (ActionResourceManager.Ninja.HutonTimer.TotalSeconds <= 0)
+                return false;
+
+            if ((uint)Spells.Ten.Charges == Spells.Ten.MaxCharges)
+                return false;
+
+            return await Spells.Hide.Cast(Core.Me);
+
+        }
         
     }
 }
